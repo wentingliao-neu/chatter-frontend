0944a6f04dba2d33daea5efca395311f92fd31b4
wentingliao-neu <liao.went@northeastern.edu>
2024-10-17 03:54:18 -0400

    enable token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,6 @@
-export const setToken = (token: string) => {}; //localStorage.setItem("token", token);
+export const setToken = (token: string) => localStorage.setItem("token", token);
 export const getToken = () => {
-   return "";
-   //const token = localStorage.getItem("token");
-   // return token ? "Bearer " + token : "";
+   const token = localStorage.getItem("token");
+   return token ? "Bearer " + token : "";
 };
 export const clearToken = () => localStorage.clear();
